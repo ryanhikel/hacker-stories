b6eb6b2e4eb0cb78f9b76c21789772bf59cdec91
Ryan Hikel <hikelryan@gmail.com>
2024-03-12 18:06:43 -0400

    refactor List to use more concise syntax

--- src/App.tsx
+++ src/App.tsx
@@ -40,18 +40,16 @@ const Search = () => (
 
 const List = () => (
   <ul>
-    {list.map((item) => {
-      return (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      );
-    })}
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
   </ul>
 );
 
